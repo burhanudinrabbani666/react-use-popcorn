e46239ef317f2baa37e0d361b57d48dfb39cebf3
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-01-22 10:26:54 +0700

    feat: changeto https

--- src/components/movie-details.jsx
+++ src/components/movie-details.jsx
@@ -55,7 +55,7 @@ export function MovieDetails({
       async function getMovieDetail() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`,
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`,
         );
 
         const data = await res.json();
